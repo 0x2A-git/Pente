fa856a8133175a064a2a1632c3fc27600eed1550
0x2A-git <zozosteam@protonmail.com>
2021-05-24 20:12:19 +0200

    Modification Pente
    Modification du main pour utiliser les nouvelles implémentations.

--- src/Pente.java
+++ src/Pente.java
@@ -1,62 +1,119 @@
 import MG2D.*;
-import MG2D.geometrie.*;
-import MG2D.geometrie.Point;
+import math.Vecteur2;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 
 public class Pente {
 
-
-    static final byte NB_CASES = 18;
 
     public static void main(String[] args){
 
 
         Fenetre fenetrePrincipale = new Fenetre("Test", 800, 600);
 
-        final int TAILLE_CASES = (int)Math.ceil(fenetrePrincipale.getWidth() / NB_CASES);
-
-        Grille test = new Grille(15,3, fenetrePrincipale);
-
-        test.getMatrice().get(0).get(0).etendreLargeurColonne(3);
-        test.getMatrice().get(0).get(0).etendreHauteurLigne(14);
+        Jeu jeu = Jeu.getInstance();
 
 
-        test.getMatrice().get(0).get(14).etendreLargeurColonne(3);
 
-        System.out.println(test.getMatrice().toString());
+        Scene scenePrincipale = new Scene(
+                fenetrePrincipale,
+                new Grille(
+                        fenetrePrincipale.getP().getWidth(),
+                        fenetrePrincipale.getP().getHeight(),
+                        6,
+                        6
+                ));
 
-        test.dessinerDebug();
+
+        jeu.init(fenetrePrincipale, scenePrincipale);
+
+
+        scenePrincipale.getGrille().dessinerDebug().forEach(d -> fenetrePrincipale.ajouter(d));
+
+        scenePrincipale.getGrille().getMatrice().get(1).get(1).etendreColonne(4);
+        scenePrincipale.getGrille().getMatrice().get(1).get(1).etendreLigne(4);
+
+        Case caseDepart = scenePrincipale.getGrille().getMatrice().get(1).get(1);
+
+        // La position est passée par référence, ainsi lorsque la case de départ bouge la grille bouge avec
+
+
+        Plateau plateau = new Plateau();
+
+        scenePrincipale.ajouter(
+                new Vecteur2<Integer>(1,1),
+                plateau
+        );
+        plateau.getGrille().setCouleurLignes(Couleur.NOIR, Couleur.NOIR);
+
+
+        //test.getMatrice().get(0).get(0).etendreLargeurColonne(3);
+        //test.getMatrice().get(0).get(0).etendreHauteurLigne(14);
+
+
+        //test.getMatrice().get(0).get(14).etendreLargeurColonne(3);
+
+        //test.ajouterColonne();
+
+
+        //plateau.getMatrice().get(1).get(0).etendreLigne(4);
+
+        //test.ajouterLigne();
+
+        //plateau.dessiner();
 
         Clavier clavier = fenetrePrincipale.getClavier();
-        Souris souris = fenetrePrincipale.getSouris();
-
         fenetrePrincipale.addKeyListener(clavier);
 
-        // *** Partie test ***
-
-        /*
-        Carre haut = new Carre(Couleur.ROUGE, new Point(fenetrePrincipale.getMilieu().getX(),fenetrePrincipale.getMilieu().getX()), 200);
-
-        fenetrePrincipale.setBackground(Couleur.JAUNE);
-
-        fenetrePrincipale.addMouseListener(souris);
-
-        System.out.println(fenetrePrincipale.getWidth() / TAILLE_CASES);
-        for(int x = 0; x < fenetrePrincipale.getWidth(); x += TAILLE_CASES){
-            for(int y = 0; y < fenetrePrincipale.getHeight(); y += TAILLE_CASES){
-
-                Ligne ligneHorizontale = new Ligne(new Point(x, y), new Point(x + TAILLE_CASES, y ));
-                ligneHorizontale.setCouleur(Couleur.NOIR);
-                fenetrePrincipale.ajouter(ligneHorizontale);
-
-                Ligne ligneVerticale = new Ligne(new Point(x, y), new Point(x, y + TAILLE_CASES ));
-                ligneVerticale.setCouleur(Couleur.NOIR);
-                fenetrePrincipale.ajouter(ligneVerticale);
+        scenePrincipale.getGrille().addOnCaseCliqueeCallback(new Grille.OnCaseCliqueeListener() {
+            @Override
+            public void onCliquee(Case caseCliquee) {
             }
-        }*/
+        });
 
+        // Attache un callback lorsque clique gauche sur la fenêtre ( via awt )
+        fenetrePrincipale.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
 
+                System.out.println(fenetrePrincipale.getInsets());
 
+                // Conversion du clique de souris en coordonnées de la grille
+                int xGrid =
+                        scenePrincipale.getGrille().getMatrice().size() -
+                                Math.floorDiv(scenePrincipale.getGrille().getLargeur() - mouseEvent.getX(), scenePrincipale.getGrille().getLargeurCases());
+
+                int yGrid = Math.floorDiv(
+                        scenePrincipale.getGrille().getHauteur() - mouseEvent.getY() + fenetrePrincipale.getInsets().top,
+                        scenePrincipale.getGrille().getHauteurCases());
+
+                System.out.println(xGrid - 1 + ", " + yGrid);
+
+                // Trigger l'event case cliquee
+                scenePrincipale.getGrille().onCaseCliquee(new Vecteur2<Integer>(xGrid - 1, yGrid), mouseEvent);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+
+            }
+        });
         while(!clavier.getEchapTape()) {
             try {
                 Thread.sleep(200);
@@ -70,3 +127,4 @@ public class Pente {
 
     }
 }
+
