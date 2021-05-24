7b0ac69115f98fd7a88e760f5b164090c8e41c13
0x2A-git <zozosteam@protonmail.com>
2021-05-24 19:10:27 +0200

    Revert "Case Commit - Ajout de méthodes"
    This reverts commit b4e63529

--- src/Case.java
+++ src/Case.java
@@ -1,7 +1,6 @@
-import MG2D.Couleur;
+import MG2D.geometrie.Dessin;
 import math.Vecteur2;
 
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Vector;
 /**
@@ -10,51 +9,31 @@ import java.util.Vector;
 
 public class Case {
 
-    /**
-     * Interface des callbacks de Case
-     *
-     * onCaseChangeTaille - Appelée lorsque la case a changée de taille en unité de grille
-     */
-    public interface Callbacks {
+    public static interface Callbacks {
         void onCaseChangeTaille(Case caseChangee);
     }
 
-    /**
-     * Trigger qui déclenche les listeners de la case
-     * @param event - Evennement de souris d'AWT
-     */
-    public void onCaseCliquee(MouseEvent event){
-        getObjets().forEach(obj -> obj.onClique(event));
-    }
+    private ArrayList<Callbacks> onCaseCallbacks = new ArrayList<>();
 
+    private Vector<Dessin> objets = null;
 
-    private Couleur couleurFond = Couleur.MAGENTA;
+    private int hauteur = 1;
+    private int largeur = 1;
 
-    private ArrayList<Callbacks> onCaseCallbacks = null;
+    private int hauteurPixels = 0;
+    private int largeurPixels = 0;
 
-    private Vector<Acteur> objets = null;
-
-    private int hauteur = 1; // En unité de grille
-    private int largeur = 1; // En unité de grille
-
-    private int hauteurPixels = 0; // En pixels
-    private int largeurPixels = 0; // En pixels
-
-    // Position dans la grille
     private Vecteur2<Integer> position = null;
 
-    // Position relative à l'écran
-    private Vecteur2<Integer> positionEcran = null;
+
     /**
      * @param largeur - Largeur de la case en pixel
      * @param hauteur - Hauteur de la case en pixel
      * @param position - Représente la position dans la matrice de la grille
-     * @param grillePos - Position de la grille relative à l'écran
      * */
+    public Case(int largeur, int hauteur, Vecteur2<Integer> position){
 
-    public Case(int largeur, int hauteur, Vecteur2<Integer> position, Vecteur2<Integer> grillePos){
-
-        this.objets = new Vector<Acteur>();
+        this.objets = new Vector<Dessin>();
 
         this.position = new Vecteur2<Integer>(position);
 
@@ -62,38 +41,6 @@ public class Case {
         this.hauteurPixels = hauteur;
         this.largeurPixels = largeur;
 
-        this.onCaseCallbacks = new ArrayList<Callbacks>();
-
-
-        this.positionEcran = new Vecteur2<Integer>(
-                grillePos.getX() + (getLargeur() * getPosition().getX()),
-                grillePos.getY() + (getHauteur() * getPosition().getY() ));
-
-    }
-
-    /**
-     * Getter des objets sur la case
-     * @return - Retourne les objets sur la case
-     * */
-    public Vector<Acteur> getObjets() {
-        return this.objets;
-    }
-
-    /**
-     * Change la couleur de la case
-     * @param couleur - Nouvelle couleur de la case
-     * */
-    public void setCouleurFond(Couleur couleur) {
-        this.couleurFond = couleur;
-    }
-
-    /**
-     * Getter de la couleur de la case
-     * @return - Couleur de la case
-     * */
-
-    public Couleur getCouleurFond(){
-        return this.couleurFond;
     }
 
     /**
@@ -101,7 +48,7 @@ public class Case {
      * @param nouvelleLargeur - Nouvelle largeur exprimée en Pixels
      * */
     public void setLargeur(int nouvelleLargeur){
-        this.largeurPixels = nouvelleLargeur;
+        this.largeurPixels = nouvelleLargeur * getLargeurColonne();
     }
 
     /**
@@ -117,7 +64,7 @@ public class Case {
      * @param nouvelleHauteur - Nouvelle hauteur exprimée en Pixels
      * */
     public void setHauteur(int nouvelleHauteur){
-        this.hauteurPixels = nouvelleHauteur;
+        this.hauteurPixels = nouvelleHauteur * getHauteurLigne();
     }
 
     /**
@@ -140,9 +87,9 @@ public class Case {
      * Permet d'étendre la largeur de la case sur la matrice de la grille
      * @param nouvelleLargeur - Nombre de colonnes à occuper sur la grille en partant de la position X de la case
      * */
-    public void etendreColonne(int nouvelleLargeur){
+    public void etendreLargeurColonne(int nouvelleLargeur){
         this.largeur = nouvelleLargeur;
-        this.largeurPixels *= this.largeur + 1;
+        this.largeurPixels *= this.largeur;
         onCaseCallbacks.forEach(listener -> listener.onCaseChangeTaille(this));
 
     }
@@ -159,13 +106,25 @@ public class Case {
      * Permet d'étendre la hauteur de la case sur la matrice de la grille
      * @param nouvelleHauteur - Nombre de lignes à occuper sur la grille en partant de la position Y de la case
      * */
-    public void etendreLigne(int nouvelleHauteur){
+    public void etendreHauteurLigne(int nouvelleHauteur){
         this.hauteur = nouvelleHauteur;
-        this.hauteurPixels *= this.hauteur + 1;
+        this.hauteurPixels *= this.hauteur;
         onCaseCallbacks.forEach(listener -> listener.onCaseChangeTaille(this));
     }
 
+    /**
+     * Permet d'ajouter un objet qu'on peut dessiner à la case ( TODO )
+     * */
+    public void ajouter(Dessin dessin){
+        this.objets.add(dessin);
+    }
 
+    /**
+     * Permet de supprimer un objet qu'on peut dessiner et qui se trouve dans la case (TODO)
+     * */
+    public void supprimer(Dessin dessin){
+        this.objets.remove(dessin);
+    }
 
     /**
      * @param cb - Callback à ajouter lorsque la taille de la case va être affectée ( en hauteur ou en largeur )
@@ -182,13 +141,5 @@ public class Case {
 
     public Vecteur2<Integer> getPosition(){
         return this.position;
-    }
-
-    /**
-     * Getter position écran
-     * @return - Retourne la position de la case par rapport à l'écran
-     * */
-    public Vecteur2<Integer> getPositionEcran(){
-        return positionEcran;
     }
 }
