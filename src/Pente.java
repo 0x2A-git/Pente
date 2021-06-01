e272061466d749fa43b881ed08a366b10068854a
TaZzZ667 <ngtazer@gmail.com>
2021-06-01 09:48:47 +0200

    Centrage de la fenêtre de dialogue

--- src/Pente.java
+++ src/Pente.java
@@ -1,4 +1,5 @@
 import MG2D.*;
+import MG2D.geometrie.Point;
 import MG2D.geometrie.Texte;
 import math.Vecteur2;
 
@@ -110,6 +111,10 @@ public class Pente {
 
         // Dialogue des paramètres de la partie
         JDialog parametresPartieFrame = new JDialog(fenetrePrincipale, "Paramètres de la partie", true);
+        final JDialog d = new JDialog();
+        d.setSize(200,200);
+        d.setLocationRelativeTo(null);
+        d.setVisible(true);
 
         parametresPartieFrame.addWindowListener(new WindowListener() {
             @Override
@@ -393,6 +398,7 @@ public class Pente {
 
         parametresPartieFrame.getContentPane().add(parametresPartiePanel);
         parametresPartieFrame.pack();
+        parametresPartieFrame.setLocationRelativeTo(null);
         parametresPartieFrame.setVisible(true);
         while(!clavier.getEchapTape()) {
             try {
